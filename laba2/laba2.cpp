988e216b579937dcec8418341d2a21059572e090
qod3r <primculov@protonmail.ch>
2020-09-27 19:47:02 +0700

    lab2 start

---
+++ laba2/laba2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <clocale>
+using namespace std;
+
+void print_menu() {
+	system("cls");
+	int menu_item;
+	cout << "1. Задание 1\n2. Задание 2\n3. Задание 3\n4. Задание 4\n5. Задание 5\n0. Выход\n";
+}
+
+void task1() {
+	cout << "задание 1 asdasdasd \n";
+	//первое задание
+}
+
+void task2() {
+	cout << "задание 2 asdasdasdad\n";
+}
+
+int main() {
+	setlocale(LC_CTYPE, "rus"); //поддержка кириллицы в консоли
+	int menu_choice;
+	bool M = true;
+
+	while (M) {
+		print_menu();
+		cin >> menu_choice;
+		switch (menu_choice) {
+		case 0:
+			M = false; //конец цикла если юзер выбрал 0
+			break;
+		case 1:
+			task1();
+			break;
+		case 2:
+			task2();
+			break;
+		default:
+			cout << "Что-то пошло не так\n";
+			M = false;
+			break;
+		}
+		if (menu_choice != 0) { system("pause"); } //остановка выполнения для отображения результата пункта
+	}
+}
