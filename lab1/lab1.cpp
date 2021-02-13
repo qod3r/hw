6c56382f688599eba54257ed23d63294a1b210b8
qod3r <primculov@protonmail.ch>
2021-02-13 19:15:17 +0700

    😳

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#define RANDOM_RANGE (rand() % 10) + 1
 using namespace std;
 
-int * CreateArr();
-
+void CreateArr();
+void CreateData();
+void Menu();
 
 int main() {
+  Menu();
+}
 
+void Menu() {
+  int menu_choice;
+  do {
+    cout << endl << " ------------- " << endl;
+    cout << " 1 - Создать файл" << endl;
+    cout << " 2 - ранд" << endl;
+    // cout << " 3 - Отсортировать шейкером" << endl;
+    // cout << " 4 - Задание 4" << endl;
+    cout << " 0 - Выйти" << endl;
+    cout << " ------------- " << endl;
+
+    cin >> menu_choice;
+
+    switch(menu_choice) {
+      case 1:
+       CreateData();
+       break;
+      // case 2:
+      //  qrand();
+      //  break;
+      // case 3:
+      //  ExecShakerSort();
+      //  break;
+      //case 4:
+      //  task4();
+      //  break;
+      case 0:
+        cout << "Bye" << endl;
+        break;
+      default:
+        cout << "Что-то пошло не так, попробуйте снова" << endl;
+        break;
+    }
+    if (menu_choice != 0) {
+      cout << endl << "Нажмите Enter чтобы продолжить...";
+      cin.get();    
+      cin.get();    
+    }               
+  } while(menu_choice);
+}
+
+void CreateData() {
+  ofstream data("array_data");
+  srand(time(0));
+
+  int elems = RANDOM_RANGE;
+  for (int i = 0; i < elems; i++) {
+    data << RANDOM_RANGE << " ";
+  }
+  data.close();
+}
+
+void CreateArr() {
+  ifstream data("array_data");
+  
+
+  int *arr = new int[];
+  data.close();
 }
