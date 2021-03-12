0f3677b55e0ba3ca9ad66d1877a8f9bb38f6ad6e
qod3r <primculov@protonmail.ch>
2021-03-12 08:39:51 +0700

    a

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -6,54 +6,64 @@ using namespace std;
 void FileWork();
 
 int main() {
-    int menu_choice;
-    do {
-        cout << endl << " ------Выбор матрицы------ " << endl;
-        cout << " 1 - Из файла" << endl;
-        cout << " 2 - С экрана" << endl;
-        cout << " 3 - Случайная" << endl;
-        cout << " 4 - Единичная" << endl;
-        cout << " 5 - Гильберта" << endl;
-        cout << " 0 - Выйти" << endl;
-        cout << " ------------------------- " << endl;
 
-        cin >> menu_choice;
-
-        switch (menu_choice) {
-            case 1:
-                FileWork();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 0:
-                cout << "Bye" << endl;
-                break;
-            default:
-                cout << "Что-то пошло не так, попробуйте снова" << endl;
-                break;
-        }
-        if (menu_choice != 0) {
-            cout << endl << "Нажмите Enter чтобы продолжить...";
-            cin.get();
-            cin.get();
-        }
-    } while (menu_choice);
+    Matrix M();
+    // ChooseTask();
     return 0;
 }
 
-void Tasks(Matrix &M, Vector &x, const char *filename) {
+Matrix ChooseMatrix() {
+    int menu_choice;
+    int n, m;
+    char filename[50];
+    cout << endl << " ------Выбор матрицы------ " << endl;
+    cout << " 1 - Из файла" << endl;
+    cout << " 2 - С экрана" << endl;
+    cout << " 3 - Случайная" << endl;
+    cout << " 4 - Единичная" << endl;
+    cout << " 5 - Гильберта" << endl;
+    cout << " 0 - Выйти" << endl;
+    cout << " ------------------------- " << endl;
+
+    cin >> menu_choice;
+
+    cout << "Введите размерность матрицы n m: ";
+    cin >> n >> m;
+    Matrix M(n, m);
+
+    switch (menu_choice) {
+        case 1:
+            cout << "Введите название файла: ";
+            cin >> filename;
+            M.GetFile(filename);
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 0:
+            cout << "Bye" << endl;
+            break;
+        default:
+            cout << "Что-то пошло не так, попробуйте снова" << endl;
+            break;
+    }
+    if (menu_choice != 0) {
+        cout << endl << "Нажмите Enter чтобы продолжить...";
+        cin.get();
+        cin.get();
+    }
+    return M;
+}
+
+void ChooseTask() {
     int menu_choice;
     do {
-        M.GetFile(filename);
-        cout << "Текущая матрица: " << endl;
-        M.Print();
-
         cout << " ------Выбор действия------ " << endl;
         cout << " 1 - Решение СЛАУ" << endl;
         cout << " 2 - Определитель" << endl;
@@ -65,6 +75,7 @@ void Tasks(Matrix &M, Vector &x, const char *filename) {
 
         switch (menu_choice) {
             case 1:
+                Matrix M = ChooseMatrix();
                 M.UpperTriangle();
                 M.Solution(x);
                 cout << "После преобразований: " << endl;
@@ -73,6 +84,7 @@ void Tasks(Matrix &M, Vector &x, const char *filename) {
                 x.Print();
                 break;
             case 2:
+                Matrix M = ChooseMatrix();
                 M.UpperTriangle();
                 cout << "Определитель = " << M.Determinant() << endl;
                 break;
@@ -91,20 +103,4 @@ void Tasks(Matrix &M, Vector &x, const char *filename) {
             cin.get();
         }
     } while (menu_choice);
-}
-
-void FileWork() {
-    int n, m;
-    char filename[50];
-
-    cout << "Введите размерность матрицы n m: ";
-    cin >> n >> m;
-
-    cout << "Введите название файла: ";
-    cin >> filename;
-
-    Matrix M(n, m);
-    Vector x(n);
-
-    Tasks(M, x, filename);
 }
